a33089f35014e056db50aa763b76a54075e11421
AM_Erizur <50849821+Erizur@users.noreply.github.com>
2025-03-14 01:16:40 -0500

    Revert "Bug 1844241 - Remove now-dead graphics glass code. r=tnikkel,devtools-reviewers"
    This reverts commit 9aa60b822ab33dae826dbd510aa3f3099232f51d.

--- servo/components/style/values/specified/box.rs
+++ servo/components/style/values/specified/box.rs
@@ -1663,6 +1663,11 @@ pub enum Appearance {
     MozWinMediaToolbox,
     #[parse(condition = "ParserContext::chrome_rules_enabled")]
     MozWinBrowsertabbarToolbox,
+    #[parse(condition = "ParserContext::chrome_rules_enabled")]
+    MozWinBorderlessGlass,
+    /// -moz-apperance style used in setting proper glass margins.
+    #[parse(condition = "ParserContext::chrome_rules_enabled")]
+    MozWinExcludeGlass,
 
     /// Mac help button.
     #[parse(condition = "ParserContext::chrome_rules_enabled")]
@@ -1677,6 +1682,8 @@ pub enum Appearance {
     /// Windows themed window frame elements.
     #[parse(condition = "ParserContext::chrome_rules_enabled")]
     MozWindowButtonBox,
+    #[parse(condition = "ParserContext::chrome_rules_enabled")]
+    MozWindowButtonBoxMaximized,
     #[parse(condition = "ParserContext::chrome_rules_enabled")]
     MozWindowButtonClose,
     #[parse(condition = "ParserContext::chrome_rules_enabled")]
